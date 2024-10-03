4266afbd6e27dfc0034872369c292ca41662a4d0
Guilherme-N-Sa <guilherme_niclewicz@hotmail.com>
2024-10-03 20:49:44 -0300

    Implement majorityElement method using Boyer-Moore Voting Algorithm for improved efficiency

--- majority_element/python.py
+++ majority_element/python.py
@@ -4,5 +4,15 @@ import math
 
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        nums.sort() # O(nlogn) Bad solution
-        return nums[math.floor(len(nums) / 2)]
+        # nums.sort() # O(nlogn) Bad solution
+        # return nums[math.floor(len(nums) / 2)]
+
+        count = 0
+        candidate = None
+
+        for num in nums:
+            if count == 0:
+                candidate = num
+            count += 1 if num == candidate else -1
+
+        return candidate

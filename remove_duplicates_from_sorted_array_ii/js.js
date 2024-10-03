4647854c543f7fc00fd4d436164a1b84415d2962
GuilhermeSa <guilherme.saddock@olist.com>
2024-10-03 20:21:34 -0300

    Refactor removeDuplicates function for improved performance and clarity

--- remove_duplicates_from_sorted_array_ii/js.js
+++ remove_duplicates_from_sorted_array_ii/js.js
@@ -3,17 +3,13 @@
  * @return {number}
  */
 var removeDuplicates = function (nums) {
-  let k = 0;
-  prev = undefined;
-
-  for (i = 0; i < nums.length; i++) {
-    let num = nums[i];
-    if (num !== prev) {
-      prev = num;
-      nums[k] = num;
+  let k = 2; // k is the index of the last element of the new array
+  for (let i = 2; i < nums.length; i++) {
+    // if the current element is different from the last element validated two positions before
+    if (nums[i] !== nums[k - 2]) {
+      nums[k] = nums[i];
       k++;
     }
   }
-
   return k;
 };
